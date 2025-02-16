80bb86240546af9dcb1b72dbd11b521b3307b205
Aryan Babare <babreab17@gmail.com>
2025-02-16 19:43:22 +0530

    removed files

--- portu/@/components/magicui/number-ticker.tsx
+++ portu/@/components/magicui/number-ticker.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useRef } from "react";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
-import React from "react";
 import { cn } from "../../lib/utils";
 
 export default function NumberTicker({
--- portu/src/pages/Home.tsx
+++ portu/src/pages/Home.tsx
@@ -1,13 +1,6 @@
-import Navbar from "@/components/Navbar";
-import aryan from "../pics/samsung-removebg-previewshortbtm.png"
-import { AuroraBackgroundDemo } from "@/components/auroraDemo";
-import { AuroraBackground } from "@/components/aurora";
-import { CardBody, CardContainer, CardItem } from "@/components/3dcard";
 
-import { motion } from "framer-motion";
 import First from "@/components/First";
 import Second from "@/components/Second";
-import Third from "@/components/Third";
 
 
 
